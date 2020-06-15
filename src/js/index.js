eababf09636f25e3d75a2ed7fa097a5e3efe7697
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2020-06-15 14:21:42 +0200

    task-1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 /* Here goes your JS code */
+const showPopupButton = document.getElementById("show-popup-form");
+const popup = document.getElementsByClassName("popup")[0];
+const form = document.getElementsByClassName("popup-form")[0];
+const closePopupButton = document.getElementsByClassName("close-popup-form")[0];
+
+showPopupButton.onclick = function(){
+    showPopupButton.style.display = "none";
+    popup.style.display = "block";
+}
+
+closePopupButton.onclick = function(){
+    showPopupButton.style.display = "block";
+    popup.style.display = "none";
+    form.reset();
+}
