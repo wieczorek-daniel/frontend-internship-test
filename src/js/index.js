d2a2b67d4acdaf2f87f578d6a556b49576b06a4e
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2020-06-15 15:33:02 +0200

    task-2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 /* Here goes your JS code */
+// Form handling
 const showPopupButton = document.getElementById("show-popup-form");
 const popup = document.getElementsByClassName("popup")[0];
 const form = document.getElementsByClassName("popup-form")[0];
@@ -13,4 +14,36 @@ closePopupButton.onclick = function(){
     showPopupButton.style.display = "block";
     popup.style.display = "none";
     form.reset();
+}
+
+// Form validation
+const submitButton = document.getElementsByClassName("form-submit")[0];
+
+submitButton.onclick = function(){
+    let emailValue = document.getElementById("email").value;
+    let passwordValue = document.getElementById("password").value;
+    let agreementChecked = document.getElementById("agreement").checked;
+    let alertMessage = document.getElementById("alert-message");
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    if (passwordValue == ""){
+        alertMessage.parentElement.style.display = "block";
+        alertMessage.innerHTML = "You forgot about your password."
+
+        if (emailValue == ""){
+            alertMessage.innerHTML = "Fill all form fields."
+        }
+    }
+    else if (regex.test(emailValue) == false){
+        alertMessage.parentElement.style.display = "block";
+        alertMessage.innerHTML = "There's problem with your e-mail."
+    }
+    else if (agreementChecked == false){
+        alertMessage.parentElement.style.display = "block";
+        alertMessage.innerHTML = "Confirm agreements."
+    }
+    else {
+        alertMessage.parentElement.style.display = "block";
+        alertMessage.innerHTML = "That's correct."
+    }
 }
